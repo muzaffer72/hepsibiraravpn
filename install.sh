ff34d41e377a94a1ab2e04d7d61922d721efeb3d
mrtunneldo2024 <167242799+mrtunneldo2024@users.noreply.github.com>
2024-04-23 19:41:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,28 +36,28 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #sstp
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/sstp.sh && chmod +x sstp.sh && ./sstp.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/sstp.sh && chmod +x sstp.sh && ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/ssr.sh && chmod +x ssr.sh && ./ssr.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ssr.sh && chmod +x ssr.sh && ./ssr.sh
 #sodosok
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/sodosok.sh && chmod +x sodosok.sh && ./sodosok.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/sodosok.sh && chmod +x sodosok.sh && ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/wg.sh && chmod +x wg.sh && ./wg.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/wg.sh && chmod +x wg.sh && ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/ins-vt.sh && chmod +x ins-vt.sh && ./ins-vt.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ins-vt.sh && chmod +x ins-vt.sh && ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/ipsec.sh && chmod +x ipsec.sh && ./ipsec.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ipsec.sh && chmod +x ipsec.sh && ./ipsec.sh
 #br-set
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 # Set Index
 cd /home/vps/public_html
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/index.html
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtunmain/index.html
 #install ohp-server
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 cd
 #install figlet & lolcat
 sudo apt-get install figlet
