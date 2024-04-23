8f6b564aaa83f27fa23e029234c0a4292b7d13fd
mrtunneldo2024 <167242799+mrtunneldo2024@users.noreply.github.com>
2024-04-23 19:28:55 +0300

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -16,7 +16,7 @@ date
 mkdir -p /etc/trojan/
 touch /etc/trojan/akun.conf
 # install v2ray
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
@@ -491,19 +491,19 @@ systemctl enable trojan
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O add-ws "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/renew-tr.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/certv2ray.sh"
+wget -O add-ws "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/add-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/add-tr.sh"
+wget -O del-ws "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/del-ws.sh"
+wget -O del-vless "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/del-vless.sh"
+wget -O del-tr "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/del-tr.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/cek-ws.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/cek-vless.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/cek-tr.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/renew-ws.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/renew-vless.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/renew-tr.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/certv2ray.sh"
 chmod +x add-ws
 chmod +x add-vless
 chmod +x add-tr
