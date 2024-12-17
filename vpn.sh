50d71b9df5c87d6ceffb11c60de6ace47b2001e3
mrtunneldo2024 <167242799+mrtunneldo2024@users.noreply.github.com>
2024-12-17 13:11:21 +0300

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/EvoTeamMalaysia/AutoScriptVPS/main/vpn.zip
+wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
