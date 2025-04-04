d0b46119c59d80df111ab967f7dc609d9ac51a41
muzaffer72 <guzelim.batmanli@gmail.com>
2025-04-04 13:49:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root to install correctly"
+		echo "Doğru şekilde kurulum yapmak için bu betiği root/yönetici olarak çalıştırmanız gerekir"
 		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
+		echo "OpenVZ desteklenmiyor"
 		exit 1
 fi
 RED='\e[1;31m'
@@ -17,47 +17,47 @@ danger="\e[91m"
 warning="\e[93m"
 success="\e[92m"
 
-echo -e "\n[${warning}Permission$based] > Masukan Password :"
+echo -e "\n[${warning}Permission$based] > Şifreyi Girin :"
 read -r -s Password
 case "$Password" in
-  evoteammalaysia0329 )
-    echo -e "\n[${success}SUCCESS$based] > Password benar"
-    echo -e "[${warning}Output$based]  > Kamu diizinkan :)"
+  005434677197 )
+    echo -e "\n[${success}SUCCESS$based] > Şifre doğru"
+    echo -e "[${warning}Output$based]  > Sana izin var :)"
   ;;
   * )
-    echo -e "\n[${danger}ERROR$based]  > Password salah"
-    echo -e "[${warning}Output$based] > Kamu tidak diizinkan :'("
+    echo -e "\n[${danger}ERROR$based]  > Yanlış şifre"
+    echo -e "[${warning}Output$based] > sana izin verilmiyor :'("
   ;;
 esac
 
 if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
+echo "Komut Dosyası Zaten Yüklendi"
 exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 #sstp
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/sstp.sh && chmod +x sstp.sh && ./sstp.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/sstp.sh && chmod +x sstp.sh && ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ssr.sh && chmod +x ssr.sh && ./ssr.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/ssr.sh && chmod +x ssr.sh && ./ssr.sh
 #sodosok
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/sodosok.sh && chmod +x sodosok.sh && ./sodosok.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/sodosok.sh && chmod +x sodosok.sh && ./sodosok.sh
 #installwg
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/wg.sh && chmod +x wg.sh && ./wg.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/wg.sh && chmod +x wg.sh && ./wg.sh
 #install v2ray
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ins-vt.sh && chmod +x ins-vt.sh && ./ins-vt.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/ins-vt.sh && chmod +x ins-vt.sh && ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ipsec.sh && chmod +x ipsec.sh && ./ipsec.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/ipsec.sh && chmod +x ipsec.sh && ./ipsec.sh
 #br-set
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 # Set Index
 cd /home/vps/public_html
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtunmain/index.html
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/refs/heads/main/index.html
 #install ohp-server
-wget https://raw.githubusercontent.com/mrtunneldo2024/mrtun/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+wget https://raw.githubusercontent.com/muzaffer72/hepsibiraravpn/main/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 cd
 #install figlet & lolcat
 sudo apt-get install figlet
@@ -76,7 +76,7 @@ rm -f /root/ohp.sh
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
-Documentation=https://www.abidz.ga
+Documentation=https://www.onvao.net
 
 [Service]
 Type=oneshot
@@ -92,11 +92,11 @@ history -c
 echo "1.2" > /home/ver
 clear
 echo " "
-sudo hostnamectl set-hostname SETUP-BY-EVOTEAM
-figlet -c Instalation Success | lolcat
+sudo hostnamectl set-hostname aio.onvao.net
+figlet -c Kurulum Başarılı | lolcat
 echo " "
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
-echo "========================== Premium Autoscript by EvoTeam =======================" | tee -a log-install.txt
+echo "========================== Premium Otomatik Kurulum scripti=======================" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
@@ -104,7 +104,7 @@ echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442, OHP 8087"  | tee -a log-install.txt
 echo "   - Stunnel4                : 443, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080 (IP Sunucusu sınırı)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
 echo "   - Wireguard               : 7070"  | tee -a log-install.txt
@@ -122,30 +122,32 @@ echo "   - Trojan                  : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Malaysia (GMT +8)"  | tee -a log-install.txt
+echo "   >>> Sunucu Bilgileri ve Diğer Özellikler"  | tee -a log-install.txt
+echo "   - Saat Dilimi                : Europa/Istanbul(GMT +3)"  | tee -a log-install.txt
+echo "   - hostname               : aio.onvao.net"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [OFF]"  | tee -a log-install.txt
+echo "   - Otomatik Yeniden Başlatma             : [OFF]"  | tee -a log-install.txt
 echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo "   - White Label" | tee -a log-install.txt
-echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo "   - Otomatik veri Yedekleme" | tee -a log-install.txt
+echo "   - Verileri Geri Yükle" | tee -a log-install.txt
+echo "   - Süresi Dolan Hesabı Otomatik Olarak Sil" | tee -a log-install.txt
+echo "   - Çeşitli Hizmetler İçin Tam Siparişler" | tee -a log-install.txt
+echo "   - Beyaz Etiket" | tee -a log-install.txt
+echo "   - Kurulum Günlüğü --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Script By               : EvoTeamMalaysia " | tee -a log-install.txt
-echo "   - Telegram                : T.me/EvoTeamVPN"  | tee -a log-install.txt
+echo "   - Script Sahibi               : Onvao.net " | tee -a log-install.txt
+echo "   - Telegram                : T.me/onvaovpn"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "================================================================================" | tee -a log-install.txt
-echo "----------------------------- Created By EvoTeam -------------------------------" | tee -a log-install.txt
+echo "----------------------------- Muzaffer Şanlı onvao.net -------------------------------" | tee -a log-install.txt
 echo "================================================================================" | tee -a log-install.txt
 echo ""
-echo "	 Your VPS Will Be Automatical Reboot In 6 s"
+echo "	 Kurulum tamamlandı ve saat dilimi istanbul olarak ayarlanıyor 6 saniye içinde Sunucu tekrar başlatılacak."
 rm -f install.sh
+sudo timedatectl set-timezone Europe/Istanbul
 sleep 6
 reboot
