a3f1ca4a43bfcc5dfea1c027526f8c440b88dff7
muzaffer72 <guzelim.batmanli@gmail.com>
2025-04-04 13:57:53 +0300

    Update about.sh

--- about.sh
+++ about.sh
@@ -6,13 +6,13 @@ NC='\e[0m'
 
 clear
 echo -e "${RED}=================================================${NC}"
-echo -e "          ${RED}Premium Auto Script By mrtun${NC}           "
+echo -e "          ${RED}Onvaonet Premium Vpn Kurulum Scripti${NC}           "
 echo -e "${RED}=================================================${NC}"
-echo -e " System Operation Support:                     "
+echo -e " Sistem İşletim Desteği:                     "
 echo -e " - Debian 9 & Debian 10 64 Bit                 "
 echo -e " - Ubuntu 18.04 - Ubuntu 22.10 64 bit          "
-echo -e " For VPS with KVM and VMWare virtualization    "
-echo -e " No OpenVZ Virtulization Supported !!!!!!!!!   "
-echo -e " t.me/EvoTeamMalaysia "
+echo -e " KVM ve VMWare sanallaştırması olan VPS'ler için    "
+echo -e " OpenVZ Sanallaştırması Desteklenmemektedir !!!!!  "
+echo -e " https://t.me/onvaonet "
 echo -e ""
 echo -e "${RED}=================================================${NC}"
