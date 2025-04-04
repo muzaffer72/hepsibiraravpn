8e6a7cb4079eac1cd030b48194f06bcff67a88f3
muzaffer72 <guzelim.batmanli@gmail.com>
2025-04-04 15:32:39 +0300

    Update list-ssh.sh

--- list-ssh.sh
+++ list-ssh.sh
@@ -3,27 +3,27 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
+echo "VPS Kontrol Ediliyor"
 
 clear
 echo "---------------------------------------------------"
-echo "USERNAME          EXP DATE          STATUS"
+echo "KULLANICI ADI       SON KULLANMA TARİHİ    DURUM"
 echo "---------------------------------------------------"
 while read expired
 do
 AKUN="$(echo $expired | cut -d: -f1)"
 ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
-exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+exp="$(chage -l $AKUN | grep "Hesap sonlanma tarihi" | awk -F": " '{print $2}')"
 status="$(passwd -S $AKUN | awk '{print $2}' )"
 if [[ $ID -ge 1000 ]]; then
 if [[ "$status" = "L" ]]; then
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}LOCKED${NORMAL}"
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}KİLİTLİ${NORMAL}"
 else
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}UNLOCKED${NORMAL}"
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}AÇIK${NORMAL}"
 fi
 fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "---------------------------------------------------"
-echo "Account number: $JUMLAH user"
+echo "Hesap sayısı: $JUMLAH kullanıcı"
 echo "---------------------------------------------------"
